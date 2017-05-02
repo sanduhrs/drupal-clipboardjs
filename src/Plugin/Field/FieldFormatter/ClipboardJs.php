572ea5ee1e6a3959620528b7edc8325c1b6587c2
Stefan Auditor <stefan.auditor@erdfisch.de>
2017-05-02 19:27:13 +0200

    Issue #2851164 by sanduhrs: Initial D8 version import

---
+++ src/Plugin/Field/FieldFormatter/ClipboardJs.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\clipboardjs\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'clipboard' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "clipboard",
+ *   label = @Translation("Clipboard"),
+ *   field_types = {
+ *     "email",
+ *     "link",
+ *     "string",
+ *     "string_long"
+ *   }
+ * )
+ */
+class ClipboardJs extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return array(
+      // Implement settings form.
+    ) + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = [
+        '#theme' => 'clipboardjs',
+        '#text' => $this->viewValue($item),
+      ];
+    }
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // The text value has no text format assigned to it, so the user input
+    // should equal the output, including newlines.
+    return nl2br(Html::escape($item->value));
+  }
+
+}
