a4095b429f8de24ad88f604e169db912f037b741
nlisgo <nlisgo@796872.no-reply.drupal.org>
2018-03-20 18:48:40 +0800

    Issue #2950484 by Andy_Read, nlisgo, kenianbei: new Clipboard(...) should be ClipboardJS(...) in clipboard.js

--- js/clipboard.js
+++ js/clipboard.js
@@ -3,13 +3,15 @@
  * Javascript to integrate the clipboard.js library with Drupal.
  */
 
+window.ClipboardJS = window.ClipboardJS || Clipboard;
+
 (function ($, Drupal, drupalSettings) {
     'use strict';
 
     Drupal.behaviors.clipboardjs = {
         attach: function (context, settings) {
             // Initialize clipboard.js.
-            Drupal.clipboard = new Clipboard('a.clipboardjs-button, input.clipboardjs-button, button.clipboardjs-button');
+            Drupal.clipboard = new ClipboardJS('a.clipboardjs-button, input.clipboardjs-button, button.clipboardjs-button');
 
             // Process successful copy.
             Drupal.clipboard.on('success', function (e) {
